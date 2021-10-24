a29f444afa8678af54005fd3013162317f7ec589
Eduardo Laguna <eduardo.laguna@mercadolibre.cl>
2021-10-24 12:40:22 -0300

    fixes module path

--- lambda-lib/state/warm_up.go
+++ lambda-lib/state/warm_up.go
@@ -6,7 +6,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
-	"github.com/theWando/dynamoLambda/lambda-lib/logger"
+	"github.com/theWando/go-aws-lambda-dynamodb/lambda-lib/logger"
 	"go.uber.org/zap"
 )
 
