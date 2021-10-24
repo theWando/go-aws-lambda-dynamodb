e87cce23d2e509aa4bd34674ea7ed675e5dc3411
Eduardo Laguna <eduardo.laguna@mercadolibre.cl>
2021-10-24 15:55:19 -0300

    fixes field tag

--- lambda-lib/model/restaurant.go
+++ lambda-lib/model/restaurant.go
@@ -1,7 +1,7 @@
 package model
 
 type Restaurant struct {
-	Name   string              `dynamodbav:"nam" json:"name,omitempty"`
+	Name   string              `dynamodbav:"name" json:"name,omitempty"`
 	Image  string              `dynamodbav:"image" json:"image,omitempty"`
 	Themes []map[string]string `dynamodbav:"themes" json:"themes,omitempty"`
 }
